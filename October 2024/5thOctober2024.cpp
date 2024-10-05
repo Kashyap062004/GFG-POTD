cd1cc9f5ef08e1b9f5211d8a9a9bbc723eeb1666
Kashyap062004 <144687549+Kashyap062004@users.noreply.github.com>
2024-10-06 00:30:28 +0530

    Create 5thOctober2024.cpp

---
+++ October 2024/5thOctober2024.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    long long findSmallest(vector<int> &a) {
+        int n=a.size();
+        long long k=1;
+        for(int i=0;i<n;i++){
+            if(a[i]>k) return k;
+            k=k+a[i];
+        }
+        
+        return k;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        vector<int> arr;
+        int num;
+        while (ss >> num) {
+            arr.push_back(num);
+        }
+
+        Solution ob;
+        auto ans = ob.findSmallest(arr);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
