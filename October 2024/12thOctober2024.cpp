83c1c29f2d23acf248792fe6d1ad71181217a329
Kashyap062004 <144687549+Kashyap062004@users.noreply.github.com>
2024-10-12 20:34:47 +0530

    Two Smallests in Every Subarray
    Given an array of integers arr, the task is to find and return the maximum sum of the smallest and second smallest element among all possible subarrays of size greater than one. If it is not possible, then return -1.

---
+++ October 2024/12thOctober2024.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int pairWithMaxSum(vector<int>& a) {
+       int n=a.size();
+       if(n==1) return -1;
+       int k=-1e5;
+       for(int i=0;i<n-1;i++){
+           k=max(k,a[i]+a[i+1]);
+       }
+       return k;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int tc;
+    cin >> tc;
+    cin.ignore();
+
+    while (tc--) {
+        string input;
+        getline(cin, input);
+
+        stringstream ss(input);
+        vector<int> a;
+        int number;
+        while (ss >> number) {
+            a.push_back(number);
+        }
+
+        Solution obj;
+        int res = obj.pairWithMaxSum(a);
+        cout << res << endl;
+    }
+}
+// } Driver Code Ends
