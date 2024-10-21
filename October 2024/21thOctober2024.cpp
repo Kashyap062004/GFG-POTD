842c476f495eca634b94af14b782950f91d84fc6
Kashyap062004 <144687549+Kashyap062004@users.noreply.github.com>
2024-10-21 21:37:34 +0530

    Split the Array
    Given an array arr[] of integers, the task is to count the number of ways to split given array elements into two non-empty subsets such that the XOR of elements of each group is equal. Each element should belong to exactly one subset.
    Note:
    
    The answer could be very large so print it by doing modulo with 10^9 + 7. 
    Subsets with the same elements but derived from different indices are different.

---
+++ October 2024/21thOctober2024.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    int countgroup(vector<int>& a) {
+        int n=a.size();
+        int k=0;
+        for(int i:a)
+            k^=i;   
+        if(k==0) return (1<<(n-1))-1;
+        return 0;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        int ans = obj.countgroup(arr);
+        cout << ans << endl;
+    }
+}
+
+// } Driver Code Ends
