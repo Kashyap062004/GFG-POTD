7718ceaa0c015863dffde94bdb4d9c1cf99b9618
Kashyap062004 <144687549+Kashyap062004@users.noreply.github.com>
2024-10-28 13:40:11 +0530

    Remove duplicates in array
    Given an array arr consisting of positive integer numbers, remove all duplicate numbers.

---
+++ October 2024/28thOctober2024.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> removeDuplicate(vector<int>& a) {
+        set<int> s; 
+        for(auto i:a) s.insert(i); 
+        vector<int> v;
+        for(auto i:a){
+            if(s.find(i)!=s.end()){
+                 v.push_back(i);
+                 s.erase(i);
+            }
+        }
+        return v;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        vector<int> ans = obj.removeDuplicate(arr);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
