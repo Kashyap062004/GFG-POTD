bf52f6e119e44960a01b2f80ebb687eb57cf153b
Kashyap062004 <144687549+Kashyap062004@users.noreply.github.com>
2024-10-30 22:17:35 +0530

    Pairs with difference k
    Given an array arr[] of positive integers. Find the number of pairs of integers whose difference equals a given number k.
    Note: (a, b) and (b, a) are considered the same. Also, the same numbers at different indices are considered different.

---
+++ October 2024/30thOctober2024.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    /* Returns count of pairs with difference k  */
+    int countPairsWithDiffK(vector<int>& a, int k) {
+        int n=a.size(),c=0;
+        map<int,int> m;
+        for(int i:a) m[i]++;
+        for(int i=0;i<n;i++){
+            if(m[ k + a[i]]) c=c+ m[k+a[i]];
+        }
+        return c;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution ob;
+        auto ans = ob.countPairsWithDiffK(arr, k);
+        cout << ans << "\n";
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
