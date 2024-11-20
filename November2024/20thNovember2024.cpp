eab124623e8f2da36a376d06954b0c93c15db352
Kashyap062004 <144687549+Kashyap062004@users.noreply.github.com>
2024-11-20 23:52:42 +0530

    Majority Element II
    You are given an array of integer arr[] where each number represents a vote to a candidate. Return the candidates that have votes greater than one-third of the total votes, If there's not a majority vote, return an empty array. 
    
    Note: The answer should be returned in an increasing format.

---
+++ November2024/20thNovember2024.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to find the majority elements in the array
+    vector<int> findMajority(vector<int>& a) {
+        int n=a.size();
+        map<int,int> m;
+        set<int> s;
+        for(int i=0;i<n;i++) m[a[i]]++;
+        for(i:m){
+            if(i.second > n/3) s.insert(i.first);
+        }
+        if(s.size()==0) return {};
+        vector<int> v;
+        for(i:s) v.push_back(i);
+         
+        return v;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        string s;
+        getline(cin, s);
+        stringstream ss(s);
+        vector<int> nums;
+        int num;
+        while (ss >> num) {
+            nums.push_back(num);
+        }
+        Solution ob;
+        vector<int> ans = ob.findMajority(nums);
+        if (ans.empty()) {
+            cout << "[]";
+        } else {
+            for (auto &i : ans)
+                cout << i << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
